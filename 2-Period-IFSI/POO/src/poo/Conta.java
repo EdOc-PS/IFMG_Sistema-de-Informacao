29e71745c61b8c2f6aa09b38bd509bb5ef077c2d
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2023-08-31 20:34:44 -0300

    Add files via upload

--- 2-Period-IFSI/POO/src/poo/Conta.java
+++ 2-Period-IFSI/POO/src/poo/Conta.java
@@ -4,9 +4,24 @@ package poo;
 class Conta {
     // atributos
     int numero;
-    float saldo;
-    String titular;
+    double saldo;
+    String titular;   
     
-    
+    void ConsultaSaldo(){
+        System.out.println("O saldo da Conta e: "+this.saldo);
     }
+    
+    void SacarValor(double sacar){
+        if(sacar <= this.saldo){
+            this.saldo -= sacar;
+            System.out.println("Valor sacado! - Saldo final: "+this.saldo);
+        }else{
+            System.out.println("Valor insuficiente");
+        }
+    }
+    void Depositar(double deposito){
+        this.saldo += deposito;
+        System.out.println("Valor depositado! - Saldo final: "+this.saldo);
+    }
+}
 
--- 2-Period-IFSI/POO/src/poo/ContaMain.java
+++ 2-Period-IFSI/POO/src/poo/ContaMain.java
@@ -1,17 +1,47 @@
 package poo;
 
 
+import java.util.Scanner;
+
+
 public class ContaMain {
    
         public static void main(String[] args){
             Conta c1;
             c1 = new Conta();
+            int esc;
+            Scanner TEC = new Scanner(System.in);
             
-            c1.titular = "Eduardo";
-            c1.saldo = 2500;
-            c1.numero = 00001;
+                System.out.println("- Bem-vindo - \nDigite o nome do Usuario: ");
+                c1.titular = TEC.next();
+                System.out.println("Digite o numero da conta: ");
+                c1.numero = TEC.nextInt();
+                System.out.println("Digite o valor da conta: ");
+                c1.saldo = TEC.nextDouble();
+         
+            do{
+            System.out.println("Escolha: \n 1 - Consultar saldo; \n 2 - Sacar valor; \n 3 - Depositar valor; \n 4 - sair;");
+            esc = TEC.nextInt();
             
-            System.out.println(" A conta: "+c1.titular+" tem o nu"
-                    + "umero: "+c1.numero+" com saldo de: "+c1.saldo);
+                switch (esc){
+                    case 1:
+                      c1.ConsultaSaldo();
+                      break;
+                    case 2:      
+                       System.out.println("Quanto deseja sacar:");
+                       double sacar = TEC.nextDouble();
+                       c1.SacarValor(sacar); 
+                       break;
+                    case 3:
+                       System.out.println("Quanto deseja depositar:");
+                       double deposito = TEC.nextDouble();
+                       c1.Depositar(deposito);
+                       break;
+
+                    default:
+                    throw new AssertionError();
+                }
+            }while(esc != 4);
         } 
+        
 }
