8c0126f20ddd501e5a67cfe5fa62c87bc3d88013
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2023-09-20 22:29:23 -0300

    Add files via upload

--- 2-Period-IFSI/POO/src/poo/ContaMain.java
+++ 2-Period-IFSI/POO/src/poo/ContaMain.java
@@ -13,20 +13,50 @@ public class ContaMain {
         //forma 2
         Cliente cl = new Cliente();
         Cliente cl2 = new Cliente();
-        int esc;
+        Funcionario F = new Funcionario();
+        
+        int esc, esc2;
         Scanner TEC = new Scanner(System.in);
             
-            System.out.println("- Banco DODOCAS -\n 1 - Cadastrar Usuario; \n 2 = Consultar Funcionarios;");
+        do{
+            System.out.println("- Banco DODOCAS -\n1 - Cadastrar Usuario; \n2 = Consultar Funcionarios;");
             esc = TEC.nextInt();
-            while(esc != 1){
-                
-            }
-            
-        
-        
+                if(esc != 1){
+                do{
+                System.out.println("-- ! Area privada ! --\nEscolha o funcionario: \n1 - Albert Einstein; \n2 - Isaac Newton; \n3 - Sair ");
+                esc2 = TEC.nextInt();
+                if(esc2 <= 2){
+                    F.ajustarFuncionario(esc2);
+                    do{
+                        System.out.println("-- ! Area privada ! --\nEditar dados: \n1 - Consultar dados; \n2 - Dar salario; \n3 - Dar aumento; \n4 - Sair;");
+                        esc2 = TEC.nextInt();
+                        switch (esc2) {
+                            case 1:
+                                F.consultaFuncionario();
+                                break;
+                            case 2:
+                                System.out.println("Valor do salario:");
+                                double salario = TEC.nextDouble();
+                                F.salarioFuncionario(salario);
+                                break;
+                            case 3:
+                                System.out.println("Valor do aumento:");
+                                double aumento = TEC.nextDouble();
+                                F.aumentoSalario(aumento);
+                                break;
+                            case 4:
+                                System.out.println("Saindo...");
+                                break;
+                            default:
+                                throw new AssertionError();
+                        }
+                    }while(esc2 != 4); 
+                }
+              }while(esc2 != 3);
+                }}while(esc != 1);
         
             c1.setTitular(cl);
-            System.out.println("- Bem-vindo - \nCadastre o nome do Usuario: ");
+            System.out.println("--- Banco --- \nCadastre o nome do Usuario: ");
             c1.getTitular().setNome(TEC.next());
             System.out.println("Cadastre o CPF: ");
             c1.getTitular().setCpf(TEC.nextInt());
--- 2-Period-IFSI/POO/src/poo/Funcionario.java
+++ 2-Period-IFSI/POO/src/poo/Funcionario.java
@@ -5,9 +5,7 @@ public class Funcionario {
     private String nomef, sobrenomef;
     private double salariof;
     
-    public void aumentoSalario(){
-        this.salariof = (this.salariof + (this.salariof * 0.1));
-    }
+    
 
     public String getNomef() {
         return nomef;
@@ -33,7 +31,37 @@ public class Funcionario {
         this.salariof = salariof;
     }
     
+    public void aumentoSalario(double aumento){
+        if(this.salariof > 0){
+        this.salariof = (this.salariof * (1 + (aumento/100)));
+            System.out.println("Salario aumentado com sucesso!");
+        }else{
+            System.out.println("Impossivel o aumento pois o salario e 0.");
+        }
+    }
     
-   
+     public void ajustarFuncionario(int esc2){
+        if(esc2 == 1){
+                this.nomef = "Albert";
+                this.sobrenomef = "Einstein";
+               
+           }else if(esc2 == 2){
+                this.nomef = "Isaac";
+                this.sobrenomef = "Newton";
+           }
+    }
+     public void consultaFuncionario(){
+        System.out.println( this.nomef+" "+this.sobrenomef+" tem o salario de: "+this.salariof);
+    }
+     public void salarioFuncionario(double salario){
+        if(salario > 0.0){
+            this.salariof = salario;
+            System.out.println("Salario ajustado com sucesso!");
+        }else{
+            this.salariof = 0;
+            System.out.println("Salario ajustado para 0.");
+        }
+        
+   }
     
 }
