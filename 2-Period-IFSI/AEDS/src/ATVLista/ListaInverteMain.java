ae8e153350d5cca9d050e2507f5fffb3d474c12f
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2023-10-03 20:14:43 -0300

    Add files via upload

---
+++ 2-Period-IFSI/AEDS/src/ATVLista/ListaInverteMain.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ATVLista;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 0057138
+ */
+public class ListaInverteMain {
+
+    public static void main(String[] args) {
+        int tam;
+        Scanner TEC = new Scanner(System.in);
+        Random RAN = new Random();
+
+        tam = TEC.nextInt();
+
+        int[] VET = new int[tam];
+        for (int i = 0; i < tam; i++) {
+            VET[i] = RAN.nextInt(10);
+        }
+    }
+    private void inverter(int VET, int tam){
+        if(tam == 1){
+            sout
+        }
+    }
+}
