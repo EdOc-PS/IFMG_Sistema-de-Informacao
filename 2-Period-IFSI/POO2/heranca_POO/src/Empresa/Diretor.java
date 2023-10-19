43a0ae6be93006c69ad46505d61e9651cfd3dd06
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2023-10-19 20:52:53 -0300

    Add files via upload

---
+++ 2-Period-IFSI/POO2/heranca_POO/src/Empresa/Diretor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Empresa;
+
+/**
+ *
+ * @author 0057138
+ */
+public class Diretor extends Gerente {
+    
+    private int numDepartamento;
+    public Diretor(int senhaG, String nomeF, float salarioF, String cpfF) {
+        super(senhaG, nomeF, salarioF, cpfF);
+    }
+    
+    
+}
