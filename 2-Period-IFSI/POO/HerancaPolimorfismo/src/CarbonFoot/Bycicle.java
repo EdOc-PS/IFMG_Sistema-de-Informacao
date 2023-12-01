e0b47872d0365a315144926312efe5ede35819cd
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2023-12-01 20:45:10 -0300

    Add files via upload

---
+++ 2-Period-IFSI/POO/HerancaPolimorfismo/src/CarbonFoot/Bycicle.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CarbonFoot;
+
+/**
+ *
+ * @author 0057138
+ */
+public class Bycicle {
+    private String marca;
+    private int tamanhoAro;
+}
---
+++ 2-Period-IFSI/POO/HerancaPolimorfismo/src/CarbonFoot/CarbonFootprint.java
@@ -0,0 +1,10 @@
+    
+package CarbonFoot;
+
+/**
+ *
+ * @author 0057138
+ */
+public interface CarbonFootprint {
+    public int getCarbonFootprint();
+}
