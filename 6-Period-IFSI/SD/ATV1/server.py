427ee1c2ecc33aff7c438cc286bc488fd799fe74
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2025-09-11 17:09:24 -0300

    Add files via upload

--- 6-Period-IFSI/SD/ATV1/server.py
+++ 6-Period-IFSI/SD/ATV1/server.py
@@ -1,4 +1,25 @@
 import socket
+import json
+import csv
+import xml.etree.ElementTree as ET
+import yaml
+import toml
+from io import StringIO
+import ast
+
+def serialize_csv(data):
+    output = StringIO()
+    writer = csv.DictWriter(output, fieldnames=data.keys())
+    writer.writeheader()
+    writer.writerow(data)
+    return output.getvalue()
+
+def serialize_xml(data):
+    root = ET.Element("message")
+    for k, v in data.items():
+        child = ET.SubElement(root, k)
+        child.text = str(v)
+    return ET.tostring(root, encoding='unicode')
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -8,17 +29,21 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     server.listen()
     print(f"Servidor rodando em {HOST}:{PORT}...")
     
-    conn = server.accept()
+    conn, addr = server.accept()
     with conn:
-        print("- Conexão aceita -")
-        for _ in range(5):
-            # Recebe o formato
-            format_type = conn.recv(1024).decode()
-            conn.sendall(b"ACK")
-            
-            # Recebe a mensagem
-            data = conn.recv(4096).decode()
-            conn.sendall(b"ACK")
-            
-            print(f"\nMensagem recebida em {format_type}:")
-            print(data)
+        
+        # Recebe
+        data = ast.literal_eval(conn.recv(4096).decode())  # string para dicionário
+        
+        # Serializa
+        formats = {
+            "JSON": json.dumps(data),
+            "CSV": serialize_csv(data),
+            "XML": serialize_xml(data),
+            "YAML": yaml.dump(data),
+            "TOML": toml.dumps(data)
+        }
+        
+        # Devolve
+        for fmt, body in formats.items():
+            conn.sendall(f"\n{fmt}: \n{body}".encode())
