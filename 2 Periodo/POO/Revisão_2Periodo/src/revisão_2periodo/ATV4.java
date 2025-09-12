6ee48e902d0077b8debe521ae3eeaa9b02037831
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2025-09-12 00:46:28 -0300

    Add files via upload

---
+++ 2 Periodo/POO/Revisão_2Periodo/src/revisão_2periodo/ATV4.java
@@ -0,0 +1,20 @@
+
+package revisão_2periodo;
+
+import java.util.Scanner;
+
+public class ATV4 {
+        public static void main(String[] args) {
+            Scanner TEC = new Scanner(System.in);
+            int N;
+            char VET[] = new char [26];
+            for (int i = 0; i < 27; i++) {
+                VET[i] = TEC.next().charAt(0);;
+            }
+            N = TEC.nextInt();
+            for (int i = 1; i < N; i++) {
+                
+            }
+            
+        }
+}
