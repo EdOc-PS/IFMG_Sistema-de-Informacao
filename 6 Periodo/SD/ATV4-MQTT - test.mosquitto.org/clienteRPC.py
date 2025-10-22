a689873626ea6920e74fa90df066d559c46a6ead
Eduardo Octávio <94386792+EdOc-PS@users.noreply.github.com>
2025-10-21 23:16:14 -0300

    Add files via upload

---
+++ 6 Periodo/SD/ATV4-MQTT - test.mosquitto.org/clienteRPC.py
@@ -0,0 +1,20 @@
+import rpyc, sys
+from game import start_game 
+
+host = 'localhost'
+port = 18861
+
+# objeto_arquivo = open('arquivo_teste.txt')
+
+username = sys.argv[1]
+
+print(f"\nConectando ao servidor em {host}:{port}...")
+
+proxy = rpyc.connect(host, port, config={'allow_public_attrs': True})
+id, player_content = proxy.root.register(username)
+print(f"Player criado - ID: {id} - Nome: {username} - Cor: {player_content['color']}\n")
+
+#n_linhas = proxy.root.contador_linha(objeto_arquivo)
+#print('Numero de linhas no arquivo: ', n_linhas)
+
+start_game(proxy, id, player_content, username)
